ddf9494c1915cd32e7367345e83042550dc93a05
gurdiano <tiagofran2013@hotmail.com>
2026-03-28 11:09:41 -0300

    Two Sum II

---
+++ Java/TwoSumII.java
@@ -0,0 +1,20 @@
+// https://neetcode.io/problems/two-integer-sum-ii]
+
+// O(n)
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while(left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if(sum == target) return new int[]{++left, ++right};
+            
+            if(sum < target) left++;
+            else right--;
+        }
+
+        return new int[0];
+    }
+}
