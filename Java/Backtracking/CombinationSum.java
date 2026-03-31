8d797b3d5d282a7ad66d24bdceed8cc724c72b91
gurdiano <tiagofran2013@hotmail.com>
2026-03-31 17:45:35 -0300

    Combination Sum

---
+++ Java/Backtracking/CombinationSum.java
@@ -0,0 +1,30 @@
+// https://neetcode.io/problems/combination-target-sum
+
+
+class Solution {
+    List<List<Integer>> RES = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<Integer> current = new ArrayList<>();
+        backtracking(nums, target, current, 0);
+        return this.RES;
+    }
+
+    public void backtracking(int[] nums, int target, List<Integer> current, int i) {
+        if(target == 0) {
+            this.RES.add(new ArrayList<>(current));
+            return;
+        }
+        if(target < 0 || i >= nums.length) {
+            return;
+        }
+        
+        int value = nums[i];
+
+        current.add(value);
+        backtracking(nums, target - value, current, i);
+
+        current.remove(current.size() - 1);
+        backtracking(nums, target, current, i + 1);
+    }
+}
