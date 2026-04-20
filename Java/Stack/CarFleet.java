77f3003f5ce2775d6cf8b1222fdd3739be351793
gurdiano <tiagofran2013@hotmail.com>
2026-04-20 12:34:22 -0300

    Car Fleet Update

--- Java/Stack/CarFleet.java
+++ Java/Stack/CarFleet.java
@@ -2,43 +2,31 @@
 
 // O(n log n)
 class Solution {
-    private TreeMap<Integer, Integer> createTreeMap(int[] position, int[] speed) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-
+    private TreeMap<Integer, Integer> carPositionSpeedMap(int[] position, int[] speed) {
+        TreeMap<Integer, Integer> map = new TreeMap<>((a, b) -> b - a);
+        
         for(int i = 0; i < position.length; i++) {
             map.put(position[i], speed[i]);
         }
         return map;
     }
-    
-    private Deque<Float> createStack(TreeMap<Integer, Integer> map, int target){
+
+    private Deque<Float> carTimeStack(TreeMap<Integer, Integer> map, int target){
         Deque<Float> stack = new ArrayDeque<>();
 
         for(int key : map.keySet()){
-            float time = (float)(target - key) / map.get(key); 
-            stack.push(time);
+            float time = (float)(target - key) / map.get(key);
+
+            if(stack.isEmpty()) stack.push(time);
+            else if(time > stack.peek()) stack.push(time);
         }
         return stack;
     }
 
     public int carFleet(int target, int[] position, int[] speed) {
-        TreeMap<Integer, Integer> map = createTreeMap(position, speed);
-        Deque<Float> stack = createStack(map, target);
+        TreeMap<Integer, Integer> map = carPositionSpeedMap(position, speed);
+        Deque<Float> stack = carTimeStack(map, target);
 
-        List<Float> res = new ArrayList<>();
-        float current = stack.pop();
-
-        res.add(current);
-
-        while(!stack.isEmpty()){
-            float num = stack.pop();
-
-            if(num > current) {
-                res.add(num);
-                current = num;
-            }
-        }   
-
-        return res.size();
+        return stack.size();
     }
 }
