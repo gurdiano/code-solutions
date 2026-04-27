626bd7d5d53b952ae2f81c9a42dfbbc6f95dfbfc
gurdiano <tiagofran2013@hotmail.com>
2026-04-27 11:27:57 -0300

    Find Minimum in Rotated Sorted Array

---
+++ Java/Binary Search/FindMinimum.java
@@ -0,0 +1,23 @@
+// https://neetcode.io/problems/find-minimum-in-rotated-sorted-array
+
+// O(log n)
+class Solution {
+    public int findMin(int[] nums) {
+        int min = nums[0];
+
+        int l = 0, r = nums.length - 1;
+        while(l <= r) {
+            if (nums[l] < nums[r]) {
+                return Math.min(min, nums[l]);
+            }
+
+            int m = (l + r) / 2;
+            min = Math.min(min, nums[m]);
+
+            if(nums[m] >= nums[l]) l = m + 1;
+            else r = m - 1;
+        }
+
+        return min;
+    }
+}
