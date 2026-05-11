70629900a338db1860657a259cd3915339377c92
gurdiano <tiagofran2013@hotmail.com>
2026-05-11 20:32:49 -0300

    Koko Eating Bananas

---
+++ Java/Binary Search/KokoEatingBananas.java
@@ -0,0 +1,25 @@
+// https://neetcode.io/problems/eating-bananas
+
+// O(n ∗ log m)
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1, r = Arrays.stream(piles).max().getAsInt();
+        int res = r;
+
+        while(l <= r) {
+            int k = (l + r) / 2;
+
+            int total = 0;
+            for(int num : piles) total += Math.ceil((double) num / k);
+
+            if(total <= h) {
+                res = k;
+                r = k - 1;
+            }else {
+                l = k + 1;
+            }
+        }
+
+        return res;
+    }
+}
